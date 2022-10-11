ec6ab03e52dc37df238100313596a8e4d016b243
Anselm Schaefer <schaefab@basfad.basf.net>
2022-10-12 01:05:13 +0200

    add some connection to the movie-info-service

--- src/main/java/com/anselm/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/anselm/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,13 +1,18 @@
 package com.anselm.moviecatalogservice.resources;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.anselm.moviecatalogservice.models.CatalogItem;
+import com.anselm.moviecatalogservice.models.Movie;
+import com.anselm.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,7 +20,20 @@ public class MovieCatalogResource {
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		return Collections.singletonList(
-				new CatalogItem("Transformers", "Test", 4));
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234", 4),
+				new Rating("5678", 3)
+		);
+		
+		return ratings.stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+		})
+		.collect(Collectors.toList());
+		// Put them all together
+		
 	}
 }
