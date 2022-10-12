818fb3b4d3183b9df74a956c8e108f067f68444c
Anselm Schaefer <schaefab@basfad.basf.net>
2022-10-12 22:55:17 +0200

    add proper REST service

--- src/main/java/com/anselm/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/anselm/moviecatalogservice/resources/MovieCatalogResource.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,22 +14,23 @@ import org.springframework.web.client.RestTemplate;
 import com.anselm.moviecatalogservice.models.CatalogItem;
 import com.anselm.moviecatalogservice.models.Movie;
 import com.anselm.moviecatalogservice.models.Rating;
+import com.anselm.moviecatalogservice.models.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+	//only creates one instance => each call will use this!
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping("/{userId}")
-	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) { 
 		
-		RestTemplate restTemplate = new RestTemplate();
+		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 		
-		List<Rating> ratings = Arrays.asList(
-				new Rating("1234", 4),
-				new Rating("5678", 3)
-		);
-		
-		return ratings.stream().map(rating -> {
+		return ratings.getUserRating().stream().map(rating -> {
+			// For each movie ID, call movie info service and get details
 			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 			return new CatalogItem(movie.getName(), "Desc", rating.getRating());
 		})
